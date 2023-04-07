7a8a5f1f6d895bad4327b616a70a16dfff3c306f
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-04-07 16:50:00 +0600

    добавил персонажей в карты бутстрап

---
+++ task_2/main.js
@@ -0,0 +1,38 @@
+const cardColumns = document.querySelector('.card-columns');
+
+function renderCharacters(characters) {
+    characters.forEach(character => {
+        const link = document.createElement('a');
+        link.href = character.url;
+        const card = document.createElement('div');
+        card.classList = 'card';
+
+        const image = document.createElement('img');
+        image.classList = 'card-img-top';
+        image.src = character.image
+        const cardBody = document.createElement('div');
+        cardBody.classList = 'card-body';
+
+        const cardTitle = document.createElement('h4');
+        cardTitle.innerText = `${character.name}`
+        cardTitle.classList = 'card-title';
+
+        cardBody.appendChild(cardTitle);
+
+        card.appendChild(image);
+        card.appendChild(cardBody);
+
+        link.appendChild(card);
+        cardColumns.appendChild(link);
+    })
+};
+
+
+fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(characters => {
+        renderCharacters(characters.results)
+    });
+
+
+
