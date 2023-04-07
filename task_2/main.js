06935c717968e1be9c34344e7e4af0bdbeb2a5b1
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-04-07 19:17:50 +0600

    страница детального просмотра

--- task_2/main.js
+++ task_2/main.js
@@ -1,9 +1,10 @@
 const cardColumns = document.querySelector('.card-columns');
+const chCard = document.querySelector('ch-card')
 
 function renderCharacters(characters) {
     characters.forEach(character => {
         const link = document.createElement('a');
-        link.href = character.url;
+        link.href = 'character.html';
         const card = document.createElement('div');
         card.classList = 'card';
 
@@ -28,11 +29,49 @@ function renderCharacters(characters) {
 };
 
 
-fetch('https://rickandmortyapi.com/api/character?page=2')
+fetch('https://rickandmortyapi.com/api/character?page=20')
     .then(response => response.json())
     .then(characters => {
         renderCharacters(characters.results)
     });
+
+fetch('https://rickandmortyapi.com/api/character/21')
+    .then(response => response.json())
+    .then(data => {
+        const image = document.createElement('img');
+        const cardBody = document.createElement('div');
+        const name = document.createElement('h3');
+        const status = document.createElement('p');
+        const species = document.createElement('p');
+        const gender = document.createElement('p');
+        const type = document.createElement('p');
+
+        image.classList = 'card-img-top';
+        image.src = data.image;
+
+        cardBody.classList = 'card-body';
+        name.classList = 'card-title';
+        name.innerHTML = data.name;
+        status.classList = 'card-text';
+        status.innerHTML = data.status;
+        species.classList = 'card-text';
+        species.innerHTML = data.species;
+        gender.classList = 'card-text';
+        gender.innerHTML = data.gender;
+        type.classList = 'card-text';
+        type.innerHTML = data.type;
+
+        cardBody.appendChild(name);
+        cardBody.appendChild(status);
+        cardBody.appendChild(species);
+        cardBody.appendChild(gender);
+        cardBody.appendChild(type);
+
+        chCard.appendChild('image');
+        chCard.appendChild('cardBody');
+    })
+
+
 
 
 
